bc9379c75d1717a943120406942ac86034826bf7
Shruti Jain <37197683+shrutijain90@users.noreply.github.com>
2019-09-12 14:42:41 -0700

    Update computeCost.m

--- computeCost.m
+++ computeCost.m
@@ -1,10 +1,7 @@
 function cost = computeCost(X,idx,centroids)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
 
 m = size(X,1);
 
 cost = (1/m)*sum(sum((X - centroids(idx,:)).^2));
 
 end
-
